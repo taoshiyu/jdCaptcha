b06691e60f50be47cff70787c3a3b6e25d4c755f
tsy <sss1063750852@126.com>
2026-02-03 10:27:36 +0800

    东航数字识别

--- app.py
+++ app.py
@@ -3,7 +3,15 @@ import json
 from verify.captcha_solver import JDCaptchaSolver
 from loguru import logger
 app = BustAPI()
+from ddddocr import DdddOcr
+ocr_object = DdddOcr(det=False,show_ad=False)
+import base64
 
+
+def solve_ocr(img):
+    b2_content = base64.b64decode(img.split(',')[1].encode())
+    result = ocr_object.classification(b2_content)
+    return result
 
 
 @app.get('/test')
@@ -27,6 +35,19 @@ def captcha_solve():
         return {'code':1,'msg':'失败'}
     else:
         return {'code':0,'msg':'成功','data':result}
+
+
+@app.post('/solve_ocr')
+def ocr_solve():
+    data = request.data.decode('utf-8')
+    data = json.loads(data)
+    try:
+        kaptcha = data.get('kaptcha')
+        ocr_result = solve_ocr(kaptcha)
+        return {'code': 0, 'msg': '','data':ocr_result}
+    except Exception as e:
+        return {'code':2,'msg':str(e),'data':None}
+
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0',port=5051)
